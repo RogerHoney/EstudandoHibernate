47340af0624529fa40af677a9eed4d877c0387ab
RogerHoney <roger.honeys@gmail.com>
2017-05-12 10:01:32 -0300

    Gerar Tabelas

---
+++ src/GerarTabelas.java
@@ -0,0 +1,13 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GerarTabelas {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.
+				createEntityManagerFactory("tarefas");
+				factory.close();
+
+	}
+
+}
